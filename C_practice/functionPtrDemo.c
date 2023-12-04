fb6470ac114eda6dcde02110f26ded6ac7027c0d
Akash Chaturvedi <akashchaturvedi@Akashs-Laptop.local>
2023-12-03 20:29:50 -0500

    pointers/function pointers experiments

---
+++ C_practice/functionPtrDemo.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef void (*EventCallback) (void*);
+
+typedef struct {
+    void* data;
+    EventCallback callback;
+} Event;
+
+void process_event(Event event)
+{
+    event.callback(event.data);
+}
+
+void handle_mouse_click(void* data)
+{
+    int* coords = (int*)data;
+    printf("Mouse clicked at (%d. %d)", coords[0], coords[1]);
+}
+
+void handle_keyboard_press(void* data)
+{
+    int key = *(int*)data;
+    printf("Keyboard key pressed! %d\n", key);
+}
+
+int main()
+{
+    int mouse_coords[] = {10,20};
+    int key = 27;
+
+    Event mouse_event = {mouse_coords, handle_mouse_click};
+    Event keyboard_event = {key, handle_keyboard_press};
+
+    process_event(mouse_event);
+    process_event(keyboard_event);
+    return 0;
+}
--- C_practice/pointers_practice.c
+++ C_practice/pointers_practice.c
@@ -35,10 +35,45 @@ void TwoDArray(int row, int column)
     }
     Print2DArray(arr,2,3);;
 }
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findErrorNums(int* nums, int numsSize, int* returnSize) {
+    int* retArr = (int*)malloc(2*sizeof(int));
+
+  for(int i = 0; i< numsSize;i++ )
+  {
+    if (nums[abs(nums[i])-1] < 0)
+    {
+      retArr[0] = nums[i];
+    }else
+    {
+      nums[nums[i]-1] *= -1;
+    }
+  }
+  for(int i =0; i< numsSize; i++)
+  {
+    if(nums[i] > 0)
+    {
+      retArr[1] = i+1;
+    }
+  }
+    return retArr;
+}
+
 int main()
 {
-    printf("Hello!\n");
-    TwoDArray(2,3);
+    int nums[3] = {2,3,2};
+    int numsSize = 3;
+    int* returnSize;
+    int* ret = findErrorNums(nums, numsSize, returnSize);
+    printf("%d ",ret[0]);
+    printf("%d ",ret[1]);
+ }
+// int main()
+// {
+//     printf("Hello!\n");
+//     TwoDArray(2,3);
 
-    return 0;
-}
+//     return 0;
